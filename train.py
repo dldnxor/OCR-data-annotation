217f334da021248bce587256185d051e7f0158cb
Alias26 <khmjuhee979@gmail.com>
2022-12-07 19:45:09 +0900

    add:wandb log

--- train.py
+++ train.py
@@ -15,37 +15,54 @@ from east_dataset import EASTDataset
 from dataset import SceneTextDataset
 from model import EAST
 
+import wandb
+
 
 def parse_args():
     parser = ArgumentParser()
 
     # Conventional args
-    parser.add_argument('--data_dir', type=str,
-                        default=os.environ.get('SM_CHANNEL_TRAIN', '../input/data/ICDAR17_Korean'))
-    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR',
-                                                                        'trained_models'))
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        default=os.environ.get("SM_CHANNEL_TRAIN", "../input/data/ICDAR17_Korean"),
+    )
+    parser.add_argument(
+        "--model_dir", type=str, default=os.environ.get("SM_MODEL_DIR", "trained_models")
+    )
 
-    parser.add_argument('--device', default='cuda' if cuda.is_available() else 'cpu')
-    parser.add_argument('--num_workers', type=int, default=4)
+    parser.add_argument("--device", default="cuda" if cuda.is_available() else "cpu")
+    parser.add_argument("--num_workers", type=int, default=4)
 
-    parser.add_argument('--image_size', type=int, default=1024)
-    parser.add_argument('--input_size', type=int, default=512)
-    parser.add_argument('--batch_size', type=int, default=12)
-    parser.add_argument('--learning_rate', type=float, default=1e-3)
-    parser.add_argument('--max_epoch', type=int, default=200)
-    parser.add_argument('--save_interval', type=int, default=5)
+    parser.add_argument("--image_size", type=int, default=1024)
+    parser.add_argument("--input_size", type=int, default=512)
+    parser.add_argument("--batch_size", type=int, default=12)
+    parser.add_argument("--learning_rate", type=float, default=1e-3)
+    parser.add_argument("--max_epoch", type=int, default=200)
+    parser.add_argument("--save_interval", type=int, default=5)
+    # parser.add_argument("-a", "--augments_name", type=str)
 
     args = parser.parse_args()
 
     if args.input_size % 32 != 0:
-        raise ValueError('`input_size` must be a multiple of 32')
+        raise ValueError("`input_size` must be a multiple of 32")
 
     return args
 
 
-def do_training(data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
-                learning_rate, max_epoch, save_interval):
-    dataset = SceneTextDataset(data_dir, split='train', image_size=image_size, crop_size=input_size)
+def do_training(
+    data_dir,
+    model_dir,
+    device,
+    image_size,
+    input_size,
+    num_workers,
+    batch_size,
+    learning_rate,
+    max_epoch,
+    save_interval,
+):
+    dataset = SceneTextDataset(data_dir, split="train", image_size=image_size, crop_size=input_size)
     dataset = EASTDataset(dataset)
     num_batches = math.ceil(len(dataset) / batch_size)
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
@@ -61,7 +78,7 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
         epoch_loss, epoch_start = 0, time.time()
         with tqdm(total=num_batches) as pbar:
             for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
-                pbar.set_description('[Epoch {}]'.format(epoch + 1))
+                pbar.set_description("[Epoch {}]".format(epoch + 1))
 
                 loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
                 optimizer.zero_grad()
@@ -73,28 +90,42 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
 
                 pbar.update(1)
                 val_dict = {
-                    'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
-                    'IoU loss': extra_info['iou_loss']
+                    "Cls loss": extra_info["cls_loss"],
+                    "Angle loss": extra_info["angle_loss"],
+                    "IoU loss": extra_info["iou_loss"],
                 }
                 pbar.set_postfix(val_dict)
-
+        wandb.log(
+            {
+                "epoch": epoch,
+                "epoch_loss": epoch_loss,
+                "Cls loss": extra_info["cls_loss"],
+                "Angle loss": extra_info["angle_loss"],
+                "IoU loss": extra_info["iou_loss"],
+            }
+        )
         scheduler.step()
 
-        print('Mean loss: {:.4f} | Elapsed time: {}'.format(
-            epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
+        print(
+            "Mean loss: {:.4f} | Elapsed time: {}".format(
+                epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)
+            )
+        )
 
         if (epoch + 1) % save_interval == 0:
             if not osp.exists(model_dir):
                 os.makedirs(model_dir)
 
-            ckpt_fpath = osp.join(model_dir, 'latest.pth')
+            ckpt_fpath = osp.join(model_dir, "latest.pth")
             torch.save(model.state_dict(), ckpt_fpath)
 
 
 def main(args):
+    run = wandb.init(project="data-ann", entity="cv_09_dataannotation", name="your_augments")
+    wandb.config.update(args)
     do_training(**args.__dict__)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     main(args)
